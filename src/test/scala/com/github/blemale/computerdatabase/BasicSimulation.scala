28adfbcdb527792699ec9562ec60e89afee2a2c4
ble <ble@lesfurets.com>
2015-12-08 14:36:34 +0100

    Step 2: PageObject Solution
    * Use PageObject pattern
    
    Conflicts:
    	src/test/scala/com/github/blemale/computerdatabase/BasicSimulation.scala

--- src/test/scala/com/github/blemale/computerdatabase/BasicSimulation.scala
+++ src/test/scala/com/github/blemale/computerdatabase/BasicSimulation.scala
@@ -1,22 +1,50 @@
-package com.github.blemale.computerdatabase // The optional package.
+package com.github.blemale.computerdatabase
 
 import io.gatling.core.Predef._
-import io.gatling.core.structure.{ScenarioBuilder, ChainBuilder}
 import io.gatling.http.Predef._
-import scala.concurrent.duration._
 
-class BasicSimulation extends Simulation { // The class declaration. Note that it extends Simulation.
+class BasicSimulation extends Simulation {
 
   object Search {
-    val search: ChainBuilder = ??? // Extract Search chain, i.e. exec(http(...)).exec(...)...
+    val search =
+      exec(http("Home")
+        .get("/"))
+        .pause(2)
+        .exec(http("Search")
+          .get("/computers?f=macbook"))
+        .pause(4)
+        .exec(http("Select")
+          .get("/computers/516"))
+        .pause(3)
   }
 
   object Browse {
-    val browse: ChainBuilder = ??? // Extract Browse chain
+    val browse =
+      exec(http("Home")
+        .get("/"))
+        .pause(2)
+        .exec(http("Page 1")
+          .get("/computers?p=1"))
+        .pause(2)
+        .exec(http("Page 2")
+          .get("/computers?p=2"))
+        .pause(2)
+        .exec(http("Page 3")
+          .get("/computers?p=3"))
+        .pause(2)
   }
 
   object Edit {
-    val edit: ChainBuilder = ??? // Extract Edit chain
+    val edit =
+      exec(http("Edit")
+        .get("/computers/new"))
+        .pause(6)
+        .exec(http("Add")
+          .post("/computers")
+          .formParam("name", "Amiga 2000")
+          .formParam("introduced", "1987-03-01")
+          .formParam("discontinued", "1991-01-01")
+          .formParam("company", "6"))
   }
 
   val httpProtocol = http
@@ -25,42 +53,10 @@ class BasicSimulation extends Simulation { // The class declaration. Note that i
 
   val uri1 = "http://computer-database.gatling.io"
 
-  val scn = scenario("BasicSimulation") // The scenario definition.
-		// Search
-		.exec(http("request_0") // A HTTP request, named request_0. This name will be displayed in the final reports.
-			.get("/")) // The url this request targets with the GET method.
-		.pause(2) // Some pause/think time.
-		.exec(http("request_1")
-			.get("/computers?f=macbook"))
-		.pause(4)
-		.exec(http("request_2")
-			.get("/computers/516"))
-		.pause(3)
-		// Browse
-		.exec(http("request_3")
-			.get("/"))
-		.pause(2)
-		.exec(http("request_4")
-			.get("/computers?p=1"))
-		.pause(2)
-		.exec(http("request_5")
-			.get("/computers?p=2"))
-		.pause(2)
-		.exec(http("request_6")
-			.get("/computers?p=3"))
-		.pause(2)
-		// Edit
-		.exec(http("request_7")
-			.get("/computers/new"))
-    .pause(6)
-    .exec(http("request_8")
-      .post("/computers")
-      .formParam("name", "Amiga 2000")
-      .formParam("introduced", "1987-03-01")
-      .formParam("discontinued", "1991-01-01")
-      .formParam("company", "6"))
+  val scn = scenario("BasicSimulation")
+		.exec(Search.search)
+		.exec(Browse.browse)
+		.exec(Edit.edit)
 
-  val scnWithPageObjects: ScenarioBuilder = ??? // Rewrite scenario using these reusable business processes
-
-  setUp(scnWithPageObjects.inject(atOnceUsers(1))).protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
